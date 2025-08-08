a4d989e6656d47b8c43f205efb7076905d570cec
Heatwave0705 <17793dxbp@gmail.com>
2025-08-08 04:27:58 +0400

    stopped repeatedly prompting user to exit and added exit option to main menu

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ tasks = {}
 
 def main_menu():
     choice = input(
+        '\n'
         "1. Add a new task\n"
         "2. Delete a task\n"
         "3. Mark a task down\n"
         "4. Delete all complete tasks\n"
         "5. See all tasks\n"
+        "6. Exit program\n"
         "Enter your choice (number 1-5): "
     )
 
@@ -33,31 +35,28 @@ def starting_prompt():
     match main_menu():
         case "1":
             new_task()
+            main()
         case "2":
             delete_task()
+            main()
         case "3":
             mark_down()
+            main()
         case "4":
             delete_complete_tasks()
+            main()
         case "5":
             print_tasks()
+            main()
+        case "6":
+            quit()
         case _:
-            print("\nThat is not an integer between 1-5, please try again\n")
+            print("\nThat is not an integer between 1-6, please try again\n")
             starting_prompt()
 
-def ending_prompt():
-    match input("\nWould you like to exit the program (enter '1') or go back to the main menu (enter '2')? "):
-        case "1":
-            quit()
-        case "2":
-            main()
-        case _:
-            print("Sorry, that's not an option, please try again")
-            ending_prompt()
 
 def main():
     starting_prompt()
-    ending_prompt()
 
 if __name__ == "__main__":
     main()
